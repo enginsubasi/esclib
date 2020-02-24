b98611f6843c45fb385439ea4c7ef4948faacbb2
Engin <enginsubasi@gmail.com>
2020-02-25 01:42:30 +0300

    + File templates added.

---
+++ template/inc/generic.h
@@ -0,0 +1,19 @@
+${filecomment}
+#ifndef ${include_guard_symbol}
+#define ${include_guard_symbol}
+
+#include <stdlib.h>
+
+/* FUNCTION DEFINITIONS */
+
+/* DEFINITIONS */
+
+/* STRUCTURES */
+
+/* ENUMS */
+
+/* EXTERNS */
+
+/* FUNCTION PROTOTYPES */
+
+#endif /* ${include_guard_symbol} */
---
+++ template/src/generic.c
@@ -0,0 +1,30 @@
+/**
+  ******************************************************************************
+  *
+  * @file:      generic.c
+  * @author:    Name Surname
+  * @e-mail:    mail@mail.com
+  * @address:   github.com/enginsubasi
+  *
+  * @version:   v 0.0.1
+  * @cdate:     20/02/2020
+  * @mdate:     20/02/2020
+  * @history:   20/02/2020 Created
+  *
+  * @about:     Generic template file.
+  * @device:    Generic
+  *
+  * @content:
+  *     FUNCTIONS:
+  *         foo1            : Brief
+  *         foo2            : Brief
+  *
+  * @notes:
+  *
+  ******************************************************************************
+  */
+
+${filecomment}
+${includes}
+
+${declarations}
