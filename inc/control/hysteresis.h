af4c371ed9eeaceb07443094d248e28d0bd66eaf
Engin <enginsubasi@gmail.com>
2020-07-16 19:21:23 +0300

    + Hysteresis control files created.

---
+++ inc/control/hysteresis.h
@@ -0,0 +1,26 @@
+#ifndef HYSTERESIS_H_
+#define HYSTERESIS_H_
+
+#include <stdint.h>
+
+/* FUNCTION DEFINITIONS */
+
+/* DEFINITIONS */
+
+#define TRUE 1
+#define FALSE 0
+
+/* STRUCTURES */
+
+typedef struct
+{
+    
+} hysteresis_t;
+
+/* ENUMS */
+
+/* EXTERNS */
+
+/* FUNCTION PROTOTYPES */
+
+#endif /* HYSTERESIS_H_ */
---
+++ src/control/hysteresis.c
@@ -0,0 +1,37 @@
+/**
+  ******************************************************************************
+  *
+  * @file:      hysteresis.c
+  * @author:    Name Surname
+  * @e-mail:    mail@mail.com
+  * @address:   github.com/enginsubasi
+  *
+  * @version:   v 0.0.1
+  * @cdate:     16/07/2020
+  * @mdate:     16/07/2020
+  * @history:   16/07/2020 Created
+  *
+  * @about:     Generic template file.
+  * @device:    Generic
+  *
+  * @content:
+  *     FUNCTIONS:
+  *         foo1            : Brief
+  *         foo2            : Brief
+  *
+  * @notes:
+  *
+  ******************************************************************************
+  */
+
+#include "hysteresis.h"
+
+int8_t hysteresisInit ( hysteresis_t* driver )
+{
+    
+}
+
+uint8_t hysteresisControl ( hysteresis_t* driver, double input )
+{
+    uint8_t controlValue = FALSE;
+}
