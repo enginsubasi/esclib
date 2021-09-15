a3fff2fd02b93fc76f41ec2335ec1fa35320e58a
Engin <enginsubasi@gmail.com>
2021-09-15 14:58:47 +0300

    + search file pair are created.

---
+++ inc/search/search.h
@@ -0,0 +1,28 @@
+#ifndef SEARCH_H_
+#define SEARCH_H_
+
+#include <stdint.h>
+
+/* FUNCTION DEFINITIONS */
+
+/* DEFINITIONS */
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+/* TYPEDEFS */
+
+/* STRUCTURES */
+
+/* ENUMS */
+
+/* EXTERNS */
+
+/* FUNCTION PROTOTYPES */
+
+#endif /* SEARCH_H_ */
---
+++ src/search/search.c
@@ -0,0 +1,26 @@
+/**
+  ******************************************************************************
+  *
+  * @file:      search.c
+  * @author:    Engin Subasi
+  * @e-mail:    enginsubasi@gmail.com
+  * @address:   github.com/enginsubasi
+  *
+  * @version:   v 0.0.1
+  * @cdate:     15/09/2021
+  * @history:   15/09/2021 Created.
+  *
+  * @about:     Search function library file.
+  * @device:    Generic
+  *
+  * @content:
+  *     FUNCTIONS:
+  *         Function Name   : Description.
+  *
+  * @notes:
+  *
+  ******************************************************************************
+  */
+
+#include "search.h"
+
