92a868ce065c370ccf2141b46cc272c2a3094e57
Engin <enginsubasi@gmail.com>
2022-03-25 12:42:11 +0300

    + hc595 driver. platform independent

---
+++ drv/hc595_drv.c
@@ -0,0 +1,109 @@
+/**
+  ******************************************************************************
+  *
+  * @file:      hc595.c
+  * @author:    Engin Subasi
+  * @email:     enginsubasi@gmail.com
+  * @address:   github.com/enginsubasi
+  *
+  * @version:   v 0.0.1
+  * @cdate:     20/11/2021
+  * @history:   20/11/2021 Created
+  *
+  * @about:     Generic template file.
+  * @device:    Generic
+  *
+  * @content:
+  *     FUNCTIONS:
+  *         foo1            : Brief
+  *         foo2            : Brief
+  *
+  * @notes:
+  *
+  ******************************************************************************
+  */
+
+#include "hc595_drv.h"
+
+void hc595Init ( struct HC595_Driver *driver,
+                    uint8_t* dataPtr,
+                    uint32_t dataSize,
+                    uint8_t dlyType,
+                    uint32_t dlyCount,
+                    void ( *sckDrvFnc )( uint8_t ),
+                    void ( *rckDrvFnc )( uint8_t ),
+                    void ( *datDrvFnc )( uint8_t ),
+                    void ( *dlyMsFnc )( uint32_t ),
+                    void ( *dlyNopFnc )( uint32_t ) )
+{
+    driver->data = dataPtr;
+    driver->size = dataSize;
+    driver->dlyType = dlyType;
+    driver->dlyCount = dlyCount;
+    driver->sckDrv = sckDrvFnc;
+    driver->rckDrv = rckDrvFnc;
+    driver->datDrv = datDrvFnc;
+    driver->dlyMs = dlyMsFnc;
+    driver->dlyNop = dlyNopFnc;
+
+    driver->datDrv ( FALSE );
+    driver->sckDrv ( FALSE );
+    driver->rckDrv ( FALSE);
+
+}
+
+static void hc595DlyCtrl ( struct HC595_Driver *driver )
+{
+    if ( driver->dlyType == DLY_NO )
+    {
+        // Intentionally blank.
+    }
+    else if ( driver->dlyType == DLY_NOP )
+    {
+        driver->dlyNop ( driver->dlyCount );
+    }
+    else if ( driver->dlyType == DLY_MS )
+    {
+        driver->dlyMs ( driver->dlyCount );
+    }
+    else
+    {
+        driver->dlyType = DLY_MS;
+        driver->dlyCount = DEF_DLY_COUNT;
+    }
+}
+
+void hc595DrvLoop ( struct HC595_Driver *driver )
+{
+
+}
+
+void hc595DrvOneShoot ( struct HC595_Driver *driver )
+{
+    uint32_t i = 0;
+    uint32_t j = 0;
+
+    for ( i = 0; i < driver->size; ++i )
+    {
+        for ( j = 0; j < 8; ++j )
+        {
+            driver->datDrv ( ( driver->data[ i ] >> j ) & 0x01 );
+            hc595DlyCtrl ( driver );
+
+            driver->sckDrv ( TRUE );
+            hc595DlyCtrl ( driver );
+            driver->sckDrv ( FALSE );
+            hc595DlyCtrl ( driver );
+        }
+    }
+
+    driver->rckDrv ( TRUE );
+    hc595DlyCtrl ( driver );
+    driver->rckDrv ( FALSE );
+    hc595DlyCtrl ( driver );
+}
+
+void hc595DrvInterrupt ( struct HC595_Driver *driver )
+{
+
+}
