a9c84808cae1fedae3ac40e0da15dda105c4c8ca
Engin <enginsubasi@gmail.com>
2022-03-29 13:55:32 +0300

    + A circular buffer files are created.

---
+++ src/buffer/circBuf.c
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  *
+  * @file:      generic.c
+  * @author:    Name Surname
+  * @email:     mail@mail.com
+  * @address:   github.com/enginsubasi
+  *
+  * @version:   v 0.0.1
+  * @cdate:     20/02/2020
+  * @history:   20/02/2020 Created
+  *
+  * @about:     Generic template file.
+  * @device:    Generic
+  *
+  * @content:
+  *     FUNCTIONS:
+  *         foo1            : Brief
+  *         foo2            : Brief
+  *
+  * @notes:
+  *
+  ******************************************************************************
+  */
+
+#include "generic.h"
+
+${filecomment}
+${includes}
+
+${declarations}
