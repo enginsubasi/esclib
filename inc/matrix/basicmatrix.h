bdca9a17d46561882328dbeb1c00ff357febd637
Engin <enginsubasi@gmail.com>
2022-06-23 14:06:30 +0300

    +

--- inc/matrix/basicmatrix.h
+++ inc/matrix/basicmatrix.h
@@ -30,10 +30,13 @@
 /* FUNCTION PROTOTYPES */
 
 void threshold1D ( float* matrix, float thresholdValue, float upValue, float dwValue, uint32_t iSize );
+
 void threshold2D ( float* matrix, float thresholdValue, float upValue, float dwValue, uint32_t iSize, uint32_t ySize );
 void threshold2Du8 ( uint8_t* matrix, uint8_t thresholdValue, uint8_t upValue, uint8_t dwValue, uint32_t iSize, uint32_t ySize );
 void threshold2Du32 ( uint32_t* matrix, uint32_t thresholdValue, uint32_t upValue, uint32_t dwValue, uint32_t iSize, uint32_t ySize );
 void threshold2Di32 ( int32_t* matrix, int32_t thresholdValue, int32_t upValue, int32_t dwValue, uint32_t iSize, uint32_t ySize );
+
+void limitUpDw2D ( float* matrix, float upValue, float dwValue, uint32_t iSize, uint32_t ySize );
 
 #ifdef __cplusplus
 }
--- src/matrix/basicmatrix.c
+++ src/matrix/basicmatrix.c
@@ -145,3 +145,30 @@ void threshold2Di32 ( int32_t* matrix, int32_t thresholdValue, int32_t upValue, 
     }
 }
 
+/*
+ * @about:
+ */
+void limitUpDw2D ( float* matrix, float thresholdValue, float upValue, float dwValue, uint32_t iSize, uint32_t ySize )
+{
+    uint32_t i = 0;
+    uint32_t j = 0;
+    
+    for ( i = 0; i < iSize; ++i )
+    {
+        for ( j = 0; j < jSize; ++j )
+        {
+            if ( matrix [ ( i * iSize ) + j ] > limitUpDw2D )
+            {
+                matrix [ ( i * iSize ) + j ] = upValue;
+            }
+            else if ( matrix [ ( i * iSize ) + j ] < dwValue )
+            {
+                matrix [ ( i * iSize ) + j ] = dwValue;
+            }
+            else
+            {
+                /* Intentionally blank. */
+            }
+        }
+    }
+}
