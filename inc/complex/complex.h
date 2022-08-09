ad13f36a5fcfa6068a665f27ec059e0f2e7e2e39
Engin <enginsubasi@gmail.com>
2022-08-09 16:46:27 +0300

    + Complex sum. sub. mul. div. operations are added.

--- inc/complex/complex.h
+++ inc/complex/complex.h
@@ -35,8 +35,11 @@ typedef struct
 
 /* FUNCTION PROTOTYPES */
 
-void complexInit ( complex_t* driver, float re, float im );
-
+void complexInit ( complex_t* cprm1, float re, float im );
+void complexSum ( complex_t* cprm1, complex_t* cprm2, complex_t* result );
+void complexSub ( complex_t* cprm1, complex_t* cprm2, complex_t* result );
+void complexMul ( complex_t* cprm1, complex_t* cprm2, complex_t* result );
+void complexDiv ( complex_t* cprm1, complex_t* cprm2, complex_t* result );
 
 #ifdef __cplusplus
 }
--- src/complex/complex.c
+++ src/complex/complex.c
@@ -1,41 +1,94 @@
 /**
   ******************************************************************************
   *
-  * @file:      generic.c
-  * @author:    Name Surname
-  * @email:     mail@mail.com
+  * @file:      complex.c
+  * @author:    Engin Subasi
+  * @email:     enginsubasi@gmail.com
   * @address:   github.com/enginsubasi
   *
   * @version:   v 0.0.1
-  * @cdate:     20/02/2020
-  * @history:   20/02/2020 Created
+  * @cdate:     09/08/2022
+  * @history:   09/08/2022 Created
   *
-  * @about:     Generic template file.
+  * @about:     Complex number library.
   * @device:    Generic
   *
   * @content:
   *     FUNCTIONS:
-  *         foo1            : Brief
-  *         foo2            : Brief
+  *         complexInit     : Complex number initialization.
+  *         complexSum      : Brief
+  *         complexSub      : Brief
+  *         complexMul      : Brief
+  *         complexDiv      : Brief
   *
   * @notes:
   *
   ******************************************************************************
   */
 
-#include "generic.h"
+#include "complex.h"
 
 /**
- * @brief Circular buffer initialization
- * @param driver
- * @param buffer
- * @param capacity
- * @return Success
+ * @brief Complex number initialization
+ * @param cprm1
+ * @param re
+ * @param im
+ * @return void
  */
-void complexInit ( complex_t* driver, float re, float im )
+void complexInit ( complex_t* cprm1, float re, float im )
 {
-    driver->re = re;
-    driver->im = im;
+    cprm1->re = re;
+    cprm1->im = im;
 }
 
+/**
+ * @brief Complex number summation
+ * @param cprm1
+ * @param cprm2
+ * @param result
+ * @return void
+ */
+void complexSum ( complex_t* cprm1, complex_t* cprm2, complex_t* result )
+{
+    result->re = ( cprm1->re + cprm2->re );
+    result->im = ( cprm1->im + cprm2->im );
+}
 
+/**
+ * @brief Complex number subtraction
+ * @param cprm1
+ * @param cprm2
+ * @param result
+ * @return void
+ */
+void complexSub ( complex_t* cprm1, complex_t* cprm2, complex_t* result )
+{
+    result->re = ( cprm1->re - cprm2->re );
+    result->im = ( cprm1->im - cprm2->im );
+}
+
+/**
+ * @brief Complex number multiplication
+ * @param cprm1
+ * @param cprm2
+ * @param result
+ * @return void
+ */
+void complexMul ( complex_t* cprm1, complex_t* cprm2, complex_t* result )
+{
+    result->re = ( cprm1->re * cprm2->re ) - ( cprm1->im * cprm2->im );
+    result->im = ( cprm1->re * cprm2->im ) + ( cprm1->im * cprm2->re ) ;
+}
+
+/**
+ * @brief Complex number divide
+ * @param cprm1
+ * @param cprm2
+ * @param result
+ * @return void
+ */
+void complexDiv ( complex_t* cprm1, complex_t* cprm2, complex_t* result )
+{
+    result->re = ( ( cprm1->re * cprm2->re ) + ( cprm1->im * cprm2->im ) / ( ( cprm2->re * cprm2->re ) + ( cprm2->im * cprm2->im ) ) );
+    result->im = ( ( cprm1->im * cprm2->re ) - ( cprm1->re * cprm2->im ) / ( ( cprm2->re * cprm2->re ) + ( cprm2->im * cprm2->im ) ) );
+}
