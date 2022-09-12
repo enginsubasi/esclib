b0f9ca145e3e238fe5941ab4256229db74ae37d8
Engin <enginsubasi@gmail.com>
2022-09-12 22:58:53 +0300

    + The files are created.

---
+++ inc/communication/comat.h
@@ -0,0 +1,36 @@
+#ifndef GENERIC_H_
+#define GENERIC_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include <stdint.h>
+
+/* FUNCTION DEFINITIONS */
+
+/* DEFINITIONS */
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+/* TYPEDEFS */
+
+/* STRUCTURES */
+
+/* ENUMS */
+
+/* EXTERNS */
+
+/* FUNCTION PROTOTYPES */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GENERIC_H_ */   
---
+++ src/communication/comat.c
@@ -0,0 +1,28 @@
+/**
+  ******************************************************************************
+  *
+  * @file:      generic.c
+  * @author:    Name Surname
+  * @email:     mail@mail.com
+  * @address:   github.com/enginsubasi
+  *
+  * @version:   v 0.0.1
+  * @cdate:     20/02/2020
+  * @history:   20/02/2020 Created
+  *
+  * @about:     Generic template file.
+  * @device:    Generic
+  *
+  * @content:
+  *     FUNCTIONS:
+  *         foo1            : Brief
+  *         foo2            : Brief
+  *
+  * @notes:
+  *
+  ******************************************************************************
+  */
+
+#include "comat.h"
+
+
