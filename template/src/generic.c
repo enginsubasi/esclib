352d6c8567a692959ee4dac0e3fbb5ac43b72c23
Engin <enginsubasi@gmail.com>
2026-07-29 09:51:27 +0300

    + Doxyfile and template updated to the Doxygen comment format

--- template/src/generic.c
+++ template/src/generic.c
@@ -1,31 +1,29 @@
 /**
   ******************************************************************************
   *
-  * @file:      generic.c
-  * @author:    Name Surname
-  * @email:     mail@mail.com
-  * @address:   github.com/enginsubasi
+  * @file      generic.c
+  * @author    Name Surname <mail@mail.com>, github.com/enginsubasi
+  * @version   0.0.1
+  * @date      20/02/2020
   *
-  * @version:   v 0.0.1
-  * @cdate:     20/02/2020
-  * @history:   20/02/2020 Created
+  * @brief     Generic template file.
   *
-  * @about:     Generic template file.
-  * @device:    Generic
+  * @par Device
+  * Generic
   *
-  * @content:
-  *     FUNCTIONS:
-  *         foo1            : Brief
-  *         foo2            : Brief
-  *
-  * @notes:
+  * @par History
+  * 20/02/2020 Created @n
   *
   ******************************************************************************
   */
 
 #include "generic.h"
 
-${filecomment}
-${includes}
-
-${declarations}
+/**
+ * @brief   One sentence describing what the function does.
+ * @param[in,out] driver  Module state. Use [out] in an Init function and
+ *                        [in] in a getter that only reads.
+ * @param[in]     value   An input parameter.
+ * @return  What the caller gets back. Omit this tag for a void function.
+ * @note    Only when there is something non obvious to say. Omit otherwise.
+ */
