98fe3f2de9283d0bb2153abd9390b0d8c63d5ba9
Engin <enginsubasi@gmail.com>
2026-09-15 10:15:07 +0300

    * File banner for comsec.c, the one source file without one

---
+++ src/communication/comsec.c
@@ -0,0 +1,18 @@
+/**
+  ******************************************************************************
+  *
+  * @file      comsec.c
+  * @author    Engin Subasi <enginsubasi@gmail.com>, github.com/enginsubasi
+  * @version   0.0.1
+  * @date      13/09/2022
+  *
+  * @brief     Secure communication framework.
+  *
+  * @par Device
+  * Generic
+  *
+  * @par History
+  * 13/09/2022 Created @n
+  *
+  ******************************************************************************
+  */
